44400b4ce27cbeb78216b7723902af1e4bbe856e
Anderson Martins <tuco.anderson@gmail.com>
2020-04-09 10:46:03 -0300

    add função de criar e deletar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post('/repositories', {
+        title: `Aplicativo ${Date.now()}`,
+        url: `https://github.com/Rocketseat/bootcamp-${Date.now()}`,
+        techs: [
+          "NodeJS",
+          "Javascript",
+          "React",
+          "React Native"
+        ]
+      });
+      console.log(response.data);
+      setRepositories([...repositories, response.data]);
+    } catch (error) {
+      alert('Error in adding a new repository');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      const repositoryIndex = repositories
+        .findIndex(repository => repository.id === id);
+
+      if (repositoryIndex < 0) {
+        alert('Repository not found');
+      }
+
+      setRepositories(repositories.filter(repository => repository.id !== id));
+      console.log('Repository removed sucessfully');
+    }
+    catch (error) {
+      alert('Erro ao deletar');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(
+          repository => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        )}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
